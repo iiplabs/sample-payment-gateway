2291554976ac5a1501ec73e84224f1c3d973d352
Igor Ilyutkin <igor@iiplabs.com>
2021-05-20 15:36:56 -0400

    SPG-5
    1) updated validation to return "email" from "cardholder.email"

--- backend/src/main/java/com/iiplabs/spg/web/utils/TransactionUtil.java
+++ backend/src/main/java/com/iiplabs/spg/web/utils/TransactionUtil.java
@@ -4,8 +4,10 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 import javax.validation.ConstraintViolation;
+import javax.validation.Path;
 import javax.validation.Validation;
 import javax.validation.Validator;
+import javax.validation.Path.Node;
 
 import com.iiplabs.spg.web.model.dto.PaymentDto;
 import com.iiplabs.spg.web.model.dto.TransactionResponseDto;
@@ -17,7 +19,7 @@ public final class TransactionUtil {
 
     public static TransactionResponseDto validateTransaction(PaymentDto paymentDto) {
 		Map<String, String> errors = VALIDATOR.validate(paymentDto).stream()
-            .collect(Collectors.toMap(o -> o.getPropertyPath().toString(), ConstraintViolation::getMessage));
+            .collect(Collectors.toMap(o -> TransactionUtil.getFieldName(o.getPropertyPath()), ConstraintViolation::getMessage));
         
         TransactionStatus status = TransactionStatus.APPROVED;
         if (errors.size() > 0) {
@@ -25,6 +27,20 @@ public final class TransactionUtil {
         }
 
         return TransactionResponseDto.builder().status(status).errors(errors).build();
+    }
+
+    /**
+     * Method takes the field key from the full path
+     * for example, "email" from "cardholder.email"
+     * @param elements
+     * @return
+     */
+    private static String getFieldName(Path elements) {
+        String field = null;
+        for (Node node : elements) {
+            field = node.getName();
+        }
+        return field;
     }
 
     private TransactionUtil() {
