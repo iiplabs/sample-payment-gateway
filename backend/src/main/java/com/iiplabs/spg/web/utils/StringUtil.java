47fc0fdd5e58ae56ab6d5d00d373f4508946a5f9
Igor Ilyutkin <igor@iiplabs.com>
2021-05-21 18:18:45 -0400

    refactored Base64 to use apache codec

--- backend/src/main/java/com/iiplabs/spg/web/utils/StringUtil.java
+++ backend/src/main/java/com/iiplabs/spg/web/utils/StringUtil.java
@@ -1,36 +1,16 @@
 package com.iiplabs.spg.web.utils;
 
-import java.nio.ByteBuffer;
-import java.nio.charset.CharsetDecoder;
-import java.nio.charset.CodingErrorAction;
-import java.nio.charset.StandardCharsets;
-import java.util.Base64;
-
+import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.lang3.StringUtils;
 
 public final class StringUtil {
 
 	public static String fromBase64(String base64) throws IllegalArgumentException {
-		return fromByteArray(Base64.getDecoder().decode(base64));
-	}
-
-	public static String fromByteArray(byte[] a) throws IllegalArgumentException {
-		String r = "";
-		try {
-			CharsetDecoder decoder = StandardCharsets.US_ASCII.newDecoder();		 
-		    decoder.onMalformedInput(CodingErrorAction.REPLACE)
-		      .onUnmappableCharacter(CodingErrorAction.REPLACE)
-		      .replaceWith("?");
-		 
-		    r = decoder.decode(ByteBuffer.wrap(a)).toString();
-		} catch(Exception e) {
-			throw new IllegalArgumentException(e);
-		}
-	    return r;
+		return new String(Base64.decodeBase64(base64.getBytes()));
 	}
 
 	public static String toBase64(String v) {
-		return Base64.getEncoder().withoutPadding().encodeToString(v.getBytes());
+		return new String(Base64.encodeBase64(v.getBytes()));
 	}
 
 	public static String maskString(String source, int start, int end, char maskChar) {
