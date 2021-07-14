cff960259c10fccf6e546890ee3f168683e9d841
Igor Ilyutkin <igor@iiplabs.com>
2021-07-14 18:26:36 -0400

    added serialization tests

--- backend/src/test/java/com/iiplabs/spg/web/controllers/PaymentControllerTest.java
+++ backend/src/test/java/com/iiplabs/spg/web/controllers/PaymentControllerTest.java
@@ -1,6 +1,14 @@
 package com.iiplabs.spg.web.controllers;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.IOException;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.iiplabs.spg.web.model.dto.CardDto;
+import com.iiplabs.spg.web.model.dto.CardHolderDto;
+import com.iiplabs.spg.web.model.dto.PaymentDto;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +30,11 @@ public class PaymentControllerTest {
   @Autowired
   PaymentController paymentController;
 
+  @Autowired
+  private ObjectMapper objectMapper;
+
+  final private String testJsonPaymentDto = "{\"invoice\":\"1234567\",\"amount\":\"1299\",\"currency\":\"EUR\",\"card\":{\"pan\":\"4024007197526238\",\"expiry\":\"0624\",\"cvv\":\"789\"},\"cardholder\":{\"name\":\"First Last\",\"email\":\"test@domain.com\"}}";
+
   @Test
 	public void contextLoads() {
     //
@@ -30,6 +43,32 @@ public class PaymentControllerTest {
   @Test
   public void whenPaymentControllerInjected_thenNotNull() throws Exception {
     assertThat(paymentController).isNotNull();
+  }
+
+  @Test
+  public void serializationTest() throws IOException {
+    PaymentDto paymentDto = objectMapper.readValue(testJsonPaymentDto, PaymentDto.class);
+    final String serializedPaymentDtoAsJson = objectMapper.writeValueAsString(paymentDto);
+    assertEquals(serializedPaymentDtoAsJson, testJsonPaymentDto);
+  }
+
+  @Test
+  public void deserializationTest() throws IOException {
+    PaymentDto deserializedPaymentDto = objectMapper.readValue(testJsonPaymentDto, PaymentDto.class);
+    PaymentDto paymentDto = new PaymentDto();
+    paymentDto.setAmount("1299");
+    CardDto card = new CardDto();
+    card.setCvv("789");
+    card.setExpiry("0624");
+    card.setPan("4024007197526238");
+    paymentDto.setCard(card);
+    CardHolderDto cardHolder = new CardHolderDto();
+    cardHolder.setEmail("test@domain.com");
+    cardHolder.setName("First Last");
+    paymentDto.setCardHolder(cardHolder);
+    paymentDto.setCurrency("EUR");
+    paymentDto.setInvoice("1234567");
+    assertEquals(deserializedPaymentDto, paymentDto);
   }
 
 }
