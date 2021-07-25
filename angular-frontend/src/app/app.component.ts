047048a9facdaca8ae30a9aa6dd632a6fb42950d
Igor Ilyutkin <igor@iiplabs.com>
2021-07-24 22:38:50 -0400

    added toolbar with title, languages and search

--- angular-frontend/src/app/app.component.ts
+++ angular-frontend/src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-frontend';
+  title = 'Simple Payment Gateway'
+  titleEnglish = 'EN'
+  titleFrench = 'FR'
 }
