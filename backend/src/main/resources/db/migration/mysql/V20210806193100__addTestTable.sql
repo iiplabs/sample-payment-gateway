e421c24e43b198124ef4bca0f1223cf128e07a43
Igor Ilyutkin <igor@iiplabs.com>
2021-08-07 10:28:43 -0400

    updated migration script

--- backend/src/main/resources/db/migration/mysql/V20210806193100__addTestTable.sql
+++ backend/src/main/resources/db/migration/mysql/V20210806193100__addTestTable.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `test_table` (
+CREATE TABLE IF NOT EXISTS `test_table` (
   `id` bigint unsigned NOT NULL AUTO_INCREMENT,
   `long_column` varchar(4096),
   `optlock` int unsigned DEFAULT 0,
